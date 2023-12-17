744786ac6f262456417f7c5a5ded6554d1754e15
SuirlCore <152679258+SuirlCore@users.noreply.github.com>
2023-12-17 16:41:43 -0700

    Update

---
+++ dataInteract.py
@@ -0,0 +1,30 @@
+# function to send an SQL statement to the database
+def sendStatement(sqlInput):
+   #connect to the database
+    mydb = mysql.connector.connect (
+        host = databaseHost[0],
+        user = databaseHost[1],
+	    password = databaseHost[2],
+	    database = databaseHost[3]
+    )
+    mycursor = mydb.cursor()
+
+    #run the SQL statement
+    mycursor.execute(sqlInput)
+
+# function to send an SQL statement to the database that retrieves information back
+def sendRequest(sqlInput):
+    #connect to the database
+    mydb = mysql.connector.connect (
+        host = databaseHost[0],
+        user = databaseHost[1],
+	    password = databaseHost[2],
+	    database = databaseHost[3]
+    )
+    mycursor = mydb.cursor()
+
+    #run the SQL statement
+    mycursor.execute(sqlInput)
+    myresult = mycursor.fetchall()
+
+    return myresult
